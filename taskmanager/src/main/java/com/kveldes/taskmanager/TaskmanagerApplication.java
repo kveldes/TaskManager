ac20cb603f571e11e7da3375249eab04e52d5a6c
kveldes <kveldes@hotmail.com>
2026-05-01 14:42:37 +0300

    Initial Git Commit.

---
+++ taskmanager/src/main/java/com/kveldes/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,19 @@
+package com.kveldes.taskmanager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(
+			TaskmanagerApplication.class
+			 );
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+		logger.info("Application started");
+	}
+
+}
---
+++ taskmanager/src/test/java/com/kveldes/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kveldes.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
